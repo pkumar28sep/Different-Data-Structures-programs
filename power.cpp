90fa9c9050e2ad416cef039e482f31b054b077bb
Prabhat kumar <55682189+pkumar28sep@users.noreply.github.com>
2021-09-18 13:29:42 +0530

    uploaded program

---
+++ power.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int power(int x,int n)
+{
+    if(n==0)
+    return 1;
+    int temp=power(x,n/2);
+    temp=temp*temp;
+    if(n%2==0)
+    return temp;
+    else
+    return temp*x;
+}
+int main()
+{
+    int x,n;
+    cin>>x>>n;
+    cout<<power(x,n);
+    return 0;
+}
